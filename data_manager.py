04903b6817a6ec7936055825af50cba207aa40d8
zoltankonradi <zoltan.konradi@gmail.com>
2018-10-29 15:14:34 +0100

    add and display comments for questions and and answers delete questions fixed

--- data_manager.py
+++ data_manager.py
@@ -55,6 +55,18 @@ def answer_reader(cursor, question_id):
                     WHERE question_id = %(q_id)s ORDER BY submission_time ASC;
                    """, {'q_id': int(question_id)})
     answer_data = cursor.fetchall()
+    data = answer_comment_reader(answer_data)
+    return data
+
+
+@connection.connection_handler
+def answer_comment_reader(cursor, answer_data):
+    for row_of_answer in answer_data:
+        cursor.execute("""
+                        SELECT * FROM comment 
+                        WHERE answer_id = %(a_id)s ORDER BY submission_time DESC;
+                       """, {'a_id': int(row_of_answer.get('id'))})
+        row_of_answer['comments'] = cursor.fetchall()
     return answer_data
 
 
@@ -82,6 +94,14 @@ def post_answer(cursor, question_id, text, image):
 @connection.connection_handler
 def delete_answer(cursor, answer_id):
     cursor.execute("""
+                    DELETE FROM comment WHERE question_id = %(id)s;
+                   """,
+                   {'id': answer_id})
+    cursor.execute("""
+                    DELETE FROM comment WHERE answer_id = %(id)s;
+                   """,
+                   {'id': answer_id})
+    cursor.execute("""
                     DELETE FROM answer WHERE id = %(id)s;
                    """,
                    {'id': answer_id})
@@ -91,13 +111,40 @@ def delete_answer(cursor, answer_id):
 @connection.connection_handler
 def delete_question(cursor, question_id):
     cursor.execute("""
+                    DELETE FROM comment WHERE question_id = %(id)s;
+                   """,
+                   {'id': question_id})
+    ids_list = find_answers_id_for_question_id(question_id)
+    for i in ids_list:
+        cursor.execute("""
+                            DELETE FROM comment WHERE answer_id = %(id)s;
+                           """,
+                       {'id': i})
+    cursor.execute("""
                     DELETE FROM answer WHERE question_id = %(id)s;
+                   """,
+                   {'id': question_id})
+    cursor.execute("""
+                    DELETE FROM question_tag WHERE question_id = %(id)s;
                    """,
                    {'id': question_id})
     cursor.execute("""
                     DELETE FROM question WHERE id = %(id)s;
                    """,
                    {'id': question_id})
+
+
+@connection.connection_handler
+def find_answers_id_for_question_id(cursor, question_id):
+    cursor.execute("""
+                    SELECT id FROM answer 
+                    WHERE question_id = %(a_id)s;
+                   """, {'a_id': int(question_id)})
+    ids = cursor.fetchall()
+    ids_list = []
+    for i in ids:
+        ids_list.append(i.get('id'))
+    return ids_list
 
 
 # KZoli - Update a question.
@@ -140,3 +187,26 @@ def update_views(cursor, question_id):
     cursor.execute("""
                     UPDATE question SET view_number = view_number + 1 WHERE id = %(id)s;""",
                    {'id': int(question_id)})
+
+# KZoli - Posts a commment.
+@connection.connection_handler
+def post_comment(cursor, comment_id, comment, comment_type):
+    submission_time = util.generate_time()
+    if comment_type == 'question':
+        cursor.execute("""INSERT INTO comment (submission_time, question_id, message, edited_count) 
+                          VALUES (%(sub_time)s, %(q_id)s, %(text)s, %(edit)s);""",
+                       {'sub_time': submission_time, 'q_id': comment_id, 'text': comment, 'edit': 0})
+    else:
+        cursor.execute("""INSERT INTO comment (submission_time, answer_id, message, edited_count) 
+                          VALUES (%(sub_time)s, %(q_id)s, %(text)s, %(edit)s);""",
+                       {'sub_time': submission_time, 'q_id': comment_id, 'text': comment, 'edit': 0})
+
+# KZoli - Reads comments for questions.
+@connection.connection_handler
+def question_comment_reader(cursor, question_id):
+    cursor.execute("""
+                    SELECT submission_time, message, edited_count FROM comment 
+                    WHERE question_id = %(q_id)s ORDER BY submission_time DESC;
+                   """, {'q_id': int(question_id)})
+    comment = cursor.fetchall()
+    return comment
--- server.py
+++ server.py
@@ -12,7 +12,7 @@ def route_list():
     question_data = data_manager.question_reader(sorting)  # KZoli - Gets a sorted dictionary of data.
     """
     [{'id': 0, 'vote_number': 7, 'title': 'How to make lists in Python?',
-      'submission_time': datetime.datetime(2017, 4, 28, 8, 29)},
+      'submission_time': datetime.datetime(2017, 4, 28, 8, 29), comments: [{}, {}]},
      {'id': 1, 'vote_number': 9, 'title': 'Wordpress loading multiple jQuery Versions',
       'submission_time': datetime.datetime(2017, 4, 29, 9, 19)}]
     """
@@ -37,8 +37,9 @@ def route_ask_question():
 def route_question(question_id):
     answer = data_manager.answer_reader(question_id)
     question = data_manager.find_question_for_answer(question_id)
+    comment_question = data_manager.question_comment_reader(question_id)
     data_manager.update_views(question_id)
-    return render_template('question.html', answer=answer, question=question)
+    return render_template('question.html', answer=answer, question=question, comment_question=comment_question)
 
 
 # KZoli - Add an answer to the specific question.
@@ -51,14 +52,14 @@ def route_question_add_answer(question_id):
 
 
 # KZoli - Delete an answer.
-@app.route("/answer/<answer_id>/delete/<question_id>")
+@app.route("/answer/<int:answer_id>/delete/<question_id>")
 def route_delete_answer(answer_id, question_id):
     data_manager.delete_answer(answer_id)
     return redirect(url_for('route_question', question_id=question_id))
 
 
 # KZoli - Delete a question.
-@app.route("/<question_id>")
+@app.route("/<int:question_id>")
 def route_delete_question(question_id):
     data_manager.delete_question(question_id)
     return redirect(url_for('route_list'))
@@ -90,6 +91,27 @@ def route_question_voting(question_id, vote):
 def route_answer_voting(question_id, vote, answer_id):
     data_manager.update_votes_answer(vote, answer_id)
     return redirect(url_for('route_question', question_id=question_id))
+
+
+@app.route("/question/<question_id>/new-comment", methods=["GET", "POST"])
+def add_comment_to_question(question_id):
+    if request.method == "GET":
+        action = "add_comment_question"
+        return render_template("ask-question.html", question_id=question_id, action=action)
+    if request.method == "POST":
+        new_comment = request.form['text']
+        data_manager.post_comment(question_id, new_comment, 'question')
+        return redirect(url_for('route_question', question_id=question_id))
+
+@app.route("/answer/<answer_id>/new-comment", methods=["GET", "POST"])
+def add_comment_to_answer(answer_id):
+    if request.method == "GET":
+        action = "add_comment_answer"
+        return render_template("ask-question.html", answer_id=answer_id, action=action)
+    if request.method == "POST":
+        new_comment = request.form['text']
+        data_manager.post_comment(answer_id, new_comment, 'answer')
+        return redirect(url_for('route_list'))
 
 
 if __name__ == "__main__":
